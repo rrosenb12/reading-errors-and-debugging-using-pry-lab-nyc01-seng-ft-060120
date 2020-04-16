88b80a12f4af19498013f03383e4c25422d64d3e
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-16 23:54:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+  star_date = (rand(100000) + 400000) / 10.0
 end
 
 
@@ -10,6 +10,6 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(date)
+  puts state_log(star_date)
   date = generate_star_date
 end
