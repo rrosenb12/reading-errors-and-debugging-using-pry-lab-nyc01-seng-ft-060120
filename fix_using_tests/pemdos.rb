fbec9f68add59f235fec64168e9c0b8c69b7521f
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-16 23:57:48 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,12 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    10.times do
+      string.insert(0, "s")
+    end
+    return string
   else
     string
   end
