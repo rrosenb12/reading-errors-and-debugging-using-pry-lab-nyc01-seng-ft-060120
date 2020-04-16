128c894df2a83948bbf091606884e99db6adaad5
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-16 23:50:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,16 +1,16 @@
 # don't forget to add: require 'pry'
 
 def generate_star_date
-  $star_date = (rand(100000) + 400000) / 10.0
-  return $star_date
+  star_date = (rand(100000) + 400000) / 10.0
+  return star_date
 end
 
 
-def state_log($star_date)
-  "Captain's Log, star date #{$star_date}."
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log($star_date)
+  puts state_log(star_date)
   date = generate_star_date
 end
